811d4ae0b9102f8bf1084172caf2d99aaa13d19f
sanathrajssr <104908944+sanathrajssr@users.noreply.github.com>
2022-05-30 18:53:02 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,22 @@
+package org.college;
+
+public class College {
+	
+	public void CollegeName() {
+		System.out.println("College Name Is SATHYABAMA");
+		
+	}
+		
+		public void CollegeCode() {
+			System.out.println("College Code Is 17");
+		}
+			
+			public void CollegeRankInTamilnadu() {
+				System.out.println("College Rank Is Top 4");
+			}
+		
+	 
+
+	}
+
+
